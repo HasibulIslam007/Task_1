6838e60982e46b0a100feb16a7615c455d8a4d89
HasibulIslam007 <hasibulislam.bracu@gmail.com>
2025-08-05 18:18:24 +0600

    Done

--- js/script.js
+++ js/script.js
@@ -9,3 +9,13 @@ dropdowns.forEach((drop) => {
       content.style.display === "block" ? "none" : "block";
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggle = document.getElementById("menuToggle");
+  const nav = document.getElementById("navMenu");
+
+  toggle.addEventListener("click", function () {
+    nav.classList.toggle("open");
+    console.log("Menu toggled:", nav.classList.contains("open"));
+  });
+});
